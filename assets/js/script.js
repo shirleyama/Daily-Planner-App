6a961916566bae9a6f313730ae2b0ae966dcf417
Ama Ama <quists@hotmail.com>
2023-01-19 01:04:31 +0000

    inputs and time updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,67 @@
-/*
-  While/For Loop that loops starting at 9 and breaks at 5
-    - For each loop generate or build html timeblock row
-      • Append timeblock to container
-        º Hour
-          - A number corresponding with the hour in 12 hour format
-        º Textarea
-          - Show existing event text, if any and allow user to input event text
-        º Save Button
-          - When clicked, store/reset the event text corresponding with the hour to localStorage
-      • Increase hour by one
-      • Check if hour is past, current or future and apply corresponding css class to timeblock
+var eventHour = document.querySelector(".hour");
+var eventInput = document.querySelector("textarea");
+var timeBlocks = document.querySelector(".container");
+var toDay = document.querySelector("#currentDay");
+var saveButton = document.querySelector(".saveBtn");
+toDay.innerHTML = moment().format("dddd MMMM, Do");
 
-*/
-var slot = document.querySelector(".hour");
-var input = document.querySelector("textarea");
-var todoDesc = document.querySelector(".time-block");
-// var saveButton = document.querySelector("saveBtn");
-
-// for (var time = 0; time >= 8 || time <= 17; time++) {
-//   console.log(time);
-//   time--;
-// }
-
+// Save button function
 function saveTodos(event) {
-  var el = event.target;
-  if (el.tagName === "BUTTON") {
-    console.log("save todos");
-    saveTodo(arr);
-  }
+  event.preventDefault();
+  console.log("Save button clicked");
+  var todoText = document.querySelector("textarea").value;
+  console.log("Todo text: " + todoText);
+
+  // Save todo text to local storage
+  localStorage.setItem("todos", JSON.stringify(todoText));
+  eventInput.value = "";
+  displayTodos();
 }
-function saveTodo(arr) {
-  console.log("save todos");
-  localStorage.setItem("todos", JSON.stringify(arr));
-}
+
 function getTodos() {
   console.log("get todos");
+  //checks local storage for todos and if none returns an empty array
   return JSON.parse(localStorage.getItem("todos")) || [];
 }
 
 function displayTodos() {
   console.log("display todos");
-  // var todos = getTodos();
-
+  var todos = getTodos();
   // //clear todos
-  // output.innerHTML = "";
+  timeBlocks.innerHTML = "";
 
-  // if (!todos.length) {
-  //   output.innerHTML = "<p>No todos have been added.</p>";
-  // }
-
-  // todos.forEach(function (todo, index) {
-  //   //loop through todos
-  //   output.insertAdjacentHTML(
-  //     "afterbegin",
-  //     `
-  //   <li>
-  //       <span>${todo}</span>
-  //       <button data-index="${index}">Complete</button>
-  //   </li>
-  //   `
-  //   );
-  // });
+  if (!todos.length) {
+    timeBlocks.innerHTML = "<p>No todos have been added.</p>";
+  }
+  var currentTime = moment().format("HH");
+  var currentHour = 9;
+  for (var i = 0; i < 8; i++) {
+    var slotTime = moment().hour(currentHour).format("h a");
+    if (currentTime === slotTime) {
+      var status = "present";
+    } else if (currentTime < slotTime) {
+      var status = "future";
+    } else if (currentTime > slotTime) {
+      var status = "past";
+    }
+    console.log(slotTime);
+    var todo = todos[i] || "";
+    timeBlocks.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="row time-block">
+      <div class="col-1 hour">${slotTime}</div>
+      <div class="col-10 text-area ${status}">
+        <textarea class="description">${todo} </textarea>
+      </div>
+      <div class="col-1 button-det">
+        <button data-index="${i}" class="btn saveBtn"><i class="fas fa-save"></i></button>
+      </div>
+    </div>
+    `
+    );
+    currentHour++;
+  }
 }
 
 function addTodo(event) {
@@ -69,39 +70,22 @@ function addTodo(event) {
   if (keyPressed === 13) {
     console.log("add todo");
 
-    var todos = getTodos(); //yet if no todos will return undefined// we are going to be pushing to this, but cant push if it's an array.
-    // //this will be the array of todod we've already stored or it'll be an empty array
-    // //lets get actual value from input above.
-    var todoText = input.value;
+    var todos = getTodos();
+    var todoText = eventInput.value;
+    console.log(todoText);
 
-    if (!todoText) return; //reurn can return a value from a function or it can stop everything and breakout
-    // //our value should have text in case they just hit enter without adding text, key13 pressed will be a truthy
+    if (!todoText) return;
     todos.push(todoText);
-    saveTodo(todos);
-
-    // saveTodos(todos);
-    // input.value = "";
-    // displayTodos();
+    localStorage.setItem("todos", JSON.stringify(todos));
+    eventInput.value = "";
+    displayTodos();
   }
-} //we need to track event to see if the enter key is pressed
-
-// function deleteTodo(event) {
-//   var el = event.target;
-//   if (el.tagName === "BUTTON") {
-//     //checking to see if button clicked and not the ul
-//     var todos = getTodos();
-//     var todoIndex = el.dataset.index;
-
-//     todos.splice(todoIndex, 1);
-//     saveTodos(todos);
-//     displayTodos();
-//   }
-// }
+}
 
 function init() {
-  input.addEventListener("keydown", addTodo);
-  //if input is inside a form element would cause browser to refresh if it is clicked
-  todoDesc.addEventListener("click", saveTodos); //grabbing click event that will pass from the lis to the ul
+  eventInput.addEventListener("keydown", addTodo);
+  saveButton.addEventListener("click", saveTodos); //grabbing click event
+
   displayTodos();
 }
 
